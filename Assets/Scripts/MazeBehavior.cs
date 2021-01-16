b63fc9a649c661c645886de5ded12900978de4e6
Andrea <andrea.rosati87@yahoo.it>
2021-01-16 12:29:27 +0100

    Inizio lavoro controlli

---
+++ Assets/Scripts/MazeBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private bool gyroAvailable;
+
+    void Start()
+    {
+        gyroAvailable = Input.gyro.enabled;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gyroAvailable) // non ho un gyroscopio quindi non posso testare :(
+          transform.rotation = GyroToUnity(Input.gyro.attitude);
+
+        print(Input.acceleration.x); // delta
+
+    }
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+}
