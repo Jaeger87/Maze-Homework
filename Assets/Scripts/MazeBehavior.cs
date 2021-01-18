4150e32031bcba0d1e34c28e95b45029d80c15e5
Andrea <andrea.rosati87@yahoo.it>
2021-01-18 18:59:21 +0100

    Rotazione ok

--- Assets/Scripts/MazeBehavior.cs
+++ Assets/Scripts/MazeBehavior.cs
@@ -7,24 +7,51 @@ public class MazeBehavior : MonoBehaviour
     // Start is called before the first frame update
 
     private bool gyroAvailable;
+    public GameObject sphere;
 
     void Start()
     {
-        gyroAvailable = Input.gyro.enabled;
+        if(SystemInfo.supportsGyroscope)
+        {
+            Input.gyro.enabled = true;
+            gyroAvailable = true;
+        }
+
+        transform.Rotate(0, 0, -00.1f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(gyroAvailable) // non ho un gyroscopio quindi non posso testare :(
-          transform.rotation = GyroToUnity(Input.gyro.attitude);
+        
+        if(Input.touchCount > 0)
+        {
+            Vector3 oldPosition = transform.position;
+            //transform.RotateAround(transform.position, Vector3.right, 20 * Time.deltaTime);
+            transform.Rotate(0, 0, -00.1f);
+            Vector3 newPosition = transform.position;
+            Vector3 difference = new Vector3(newPosition.x - oldPosition.x, newPosition.y - oldPosition.y,
+                newPosition.z - oldPosition.z);
+            sphere.transform.Translate(difference);
 
-        print(Input.acceleration.x); // delta
+            
+        }
+        //print(SystemInfo.supportsGyroscope && Input.gyro.enabled);
+        if (gyroAvailable) // non ho un gyroscopio quindi non posso testare :(
+        {
+            print(Input.gyro.attitude);
+           // transform.rotation = GyroToUnity(Input.gyro.attitude);
+        }
+          
+
+        //print(Input.acceleration.x);
+
 
     }
 
     private static Quaternion GyroToUnity(Quaternion q)
     {
-        return new Quaternion(q.x, q.y, -q.z, -q.w);
+        return new Quaternion(q.y, q.x, 0, 0);
     }
 }
