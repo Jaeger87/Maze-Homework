89649e6014c18932e9837cf6a343939bac0a8424
Andrea <andrea.rosati87@yahoo.it>
2021-01-18 21:21:25 +0100

    Gira bene

--- Assets/Scripts/MazeBehavior.cs
+++ Assets/Scripts/MazeBehavior.cs
@@ -8,6 +8,11 @@ public class MazeBehavior : MonoBehaviour
 
     private bool gyroAvailable;
     public GameObject sphere;
+    private const float tolleranceGyro = 0.5f;
+    private const float maxYRotation = 25f;
+    private const float maxXRotation = 22f;
+    private float xCurrentRotation = 0;
+    private float yCurrentRotation = 0;
 
     void Start()
     {
@@ -24,27 +29,31 @@ public class MazeBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.touchCount > 0)
+        {
+            transform.Rotate(0.01f, 0, 0);
+
+        }
+
+
+        if (gyroAvailable)
+        {
+            //print(Input.gyro.attitude);
+
+            float myGyroY = Mathf.Clamp(Input.gyro.attitude.x, -tolleranceGyro, tolleranceGyro);
+            float myGyroX = Mathf.Clamp(Input.gyro.attitude.y, -tolleranceGyro, tolleranceGyro);
+
+            myGyroX = scale(-tolleranceGyro, tolleranceGyro, -maxXRotation, maxXRotation, myGyroX);
+            myGyroY = scale(-tolleranceGyro, tolleranceGyro, maxYRotation, -maxYRotation, myGyroY);
+
+
+            transform.Rotate(myGyroX - xCurrentRotation, 0, myGyroY - yCurrentRotation); 
+            xCurrentRotation = myGyroX;
+            yCurrentRotation = myGyroY;
+        }
+
         
-        if(Input.touchCount > 0)
-        {
-            Vector3 oldPosition = transform.position;
-            //transform.RotateAround(transform.position, Vector3.right, 20 * Time.deltaTime);
-            transform.Rotate(0, 0, -00.1f);
-            Vector3 newPosition = transform.position;
-            Vector3 difference = new Vector3(newPosition.x - oldPosition.x, newPosition.y - oldPosition.y,
-                newPosition.z - oldPosition.z);
-            sphere.transform.Translate(difference);
-
-            
-        }
-        //print(SystemInfo.supportsGyroscope && Input.gyro.enabled);
-        if (gyroAvailable) // non ho un gyroscopio quindi non posso testare :(
-        {
-            print(Input.gyro.attitude);
-           // transform.rotation = GyroToUnity(Input.gyro.attitude);
-        }
-          
-
         //print(Input.acceleration.x);
 
 
@@ -52,6 +61,18 @@ public class MazeBehavior : MonoBehaviour
 
     private static Quaternion GyroToUnity(Quaternion q)
     {
-        return new Quaternion(q.y, q.x, 0, 0);
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
     }
+
+
+    private float scale(float OldMin, float OldMax, float NewMin, float NewMax, float OldValue)
+    {
+
+        float OldRange = (OldMax - OldMin);
+        float NewRange = (NewMax - NewMin);
+        float NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
+
+        return (NewValue);
+    }
+
 }
