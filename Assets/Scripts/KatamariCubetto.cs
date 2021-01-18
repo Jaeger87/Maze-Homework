c70d3bb12a260984ab38200a0082f8cb8515c217
Andrea <andrea.rosati87@yahoo.it>
2021-01-18 23:12:17 +0100

    Katamari fatto

---
+++ Assets/Scripts/KatamariCubetto.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KatamariCubetto : MonoBehaviour
+{
+    private bool firstTimeSphere = true;
+    public GameObject sphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!firstTimeSphere)
+            return;
+        if(collision.gameObject.Equals(sphere))
+        {
+            this.transform.parent = sphere.transform;
+            firstTimeSphere = true;
+            Destroy(this.GetComponent<Rigidbody>());
+        }
+    }
+}
